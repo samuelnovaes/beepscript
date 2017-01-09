1a1f2c50282071ca65662222690985fd3a169dcc
Samuel Novaes Sena <samuel.novaes.96@gmail.com>
2017-01-09 00:21:31 -0200

    09/01/2017 00:21

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# beepscript
+# BeepScript
+
+> It does'nt work in Windows
+
 Script language to create wave musics
+
+# How to install
+
+```sh
+npm install -g beepscript
+```
+
+# How to compile
+```sh
+beepscript mymusic.bs
+```
+It will generate a .wav file automatically in the same directory
+
+#Syntaxe
+```javascript
+sequence {
+    /*Commands...*/
+}
+```
+#Commands
+- **beep(frequency, duration)** (Duration in seconds, frequency in Hz)
+- **beep(frequency)** (Beep for 1 second)
+- **beep()** (Beep 440 Hz for 1 second)
+- **sleep(duration)** (Duration in seconds)
+- **sleep()** (Sleep for 1 second)
+- **play(sequence, loop)** (Sequence must be betwen quotes)
+- **play(sequence)** (Play sequence only one time)
+- **note(note, octave, duration)** (Note must be betwen quotes, octave is an integer number, duration in secods)
+- **note(note, octave)** (Play a note for 1 second)
+- **note(note)** (Play a note in octave 4 for 1 second)
+- **note()** (Play the note A in octave 4 for 1 second, equivalent to beep())
+- **Supported notes:** C, C#, D, D#, E, F, F#, G, G#, A, A#, B
+
+#Comments
+```javascript
+/* This is a comment */
+```
+Comments must be between /* and */
+
+#Remember
+BeepScript isn't case sensitive and ignores any space. BeepScript doesn't use semicolons. The script below works perfectly.
+```javascript
+M AIn {
+    PLAY('sequ Ence1')
+}
+Sequence 1 {
+    Be E p()
+}
+```
+
+#Example
+mymusic.bs
+```javascript
+main {                     /* Main sequence */
+    play('sequence1', 3)   /* Play the sequence1 3 times */
+    beep(600)              /* Beep 600 Hz frequency for 1 second */
+    beep()                 /* Beep 440 Hz frequency for 1 second */
+    sleep(0.5)             /* Sleep for 0.5 seconds */
+    beep(400, 0.3)         /* Beep 400 Hz frequency for 0.3 seconds */
+    sleep()                /* Sleep for 1 second */
+    play('sequence2')      /* Play the sequence2 one time */
+    note('C#', 3, 0.5)     /* Play the note C# in octave 3 for 0.5 seconds */
+    note('B', 5)           /* Play the note B in octave 5 for 1 second */
+    note()                 /* Play the note A in octave 4 for 1 second */
+}                          /* Close main sequence */
+
+sequence1 {
+    sleep(0.5)
+    beep(600, 0.3)
+    beep(500, 0.3)
+    beep(400, 0.3)
+    beep(500, 0.3)
+}
+
+sequence2 {
+    note('C', 4, 0.5)
+    note('F#', 3)
+    note('F', 4, 0.3)
+    sleep(2) 
+}
+```
---
+++ index.js
@@ -0,0 +1,93 @@
+#!/usr/bin/env node
+var header = require("waveheader");
+var fs = require("fs");
+var path = require("path");
+
+var tone = {
+	play: function(freq, lengthInSecs) {
+		freq = freq || 400;
+		lengthInSecs = lengthInSecs || 1;
+		var cycle = Math.round(44100/freq);
+		var samplesLeft = lengthInSecs * 44100;
+		var cycles = samplesLeft/cycle;
+		var ret = [];
+		for(var i = 0; i < cycles; i++) {
+			ret = ret.concat(generateCycle(cycle, 30));
+		}
+		return ret;
+	},
+	sleep: function(lengthInSecs) {
+		lengthInSecs = lengthInSecs || 1;
+		var cycle = Math.round(44100 * lengthInSecs);
+		return generateCycle(cycle, 0);
+	}
+};
+
+var res = [];
+var fileString = fs.readFileSync(process.argv[2], "utf-8");
+fileString = ('{"'+fileString.toLowerCase().split('\n').join('').split('\t').join('').split(' ').join('').split('"').join("'").split(/\/\*.+?\*\/|\/\/.*(?=[\n\r])/g).join('').split('{').join('":"{').split('{').join('[').split('}').join(']","').split(')').join('),').split(',]').join(']')+'}').split(',"}').join('}');
+var objeto = JSON.parse(fileString);
+var main = eval(objeto["main"]);
+main.forEach(function(item){
+	res = res.concat(item);
+});
+
+
+var wavFile = path.dirname(process.argv[2])+"/"+path.parse(process.argv[2]).name+".wav";
+var writer = new fs.createWriteStream(wavFile);
+writer.write(header());
+writer.write(new Buffer(res));
+writer.end();
+console.log("Script compiled to "+wavFile);
+
+function generateCycle(cycle, volume) {
+	var data = [];
+	var tmp;
+	for(var i = 0; i < cycle; i++) {
+		tmp = volume * Math.sin((i/cycle) * Math.PI * 2);
+		data[i] = Math.round(tmp);
+	}
+	return data;
+}
+function play(index, loop){
+	loop = loop || 1;
+	var res = [];
+	var actions = eval(objeto[index]);
+	for(var i = 0; i < loop; i++){
+		actions.forEach(function(item){
+			res = res.concat(item);
+		});
+	}
+	return res;
+}
+function beep(freq, time){
+	return tone.play(freq, time);
+}
+function note(nota, octavo, time){
+	if(nota != undefined){
+		octavo = octavo == undefined ? 4 : octavo;
+		nota = nota.split(' ').join('');
+		var notas = {
+			"c": 16,
+			"c#": 17,
+			"d": 18,
+			"d#": 19,
+			"e": 21,
+			"f": 22,
+			"f#": 23,
+			"g": 25,
+			"g#": 26,
+			"a": 27,
+			"a#": 29,
+			"b": 30
+		}
+		var frequencia = notas[nota] * Math.pow(2, octavo);
+		return beep(frequencia, time);
+	}
+	else{
+		return beep(440, 1);
+	}
+}
+function sleep(time){
+	return tone.sleep(time);
+}
