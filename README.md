dd6b6d45714883e5e0fbef102ce43f7f59ff8e10
Samuel Novaes <samuel.novaes.96@gmail.com>
2017-06-12 21:21:08 -0300

    12/06/2017 21:21

--- README.md
+++ README.md
@@ -1,63 +1,40 @@
 # BeepScript
 
-Script language to create wave musics
+Create wave musics with JavaScript
 
-# How to install
+# Full Example
 
-```sh
-[sudo] npm install -g beepscript
-```
-
-# How to compile
-```sh
-beepscript mymusic.bs
-```
-It will generate a .wav file automatically in the same directory
-
-# Syntaxe
 ```javascript
-sequence {
-    /*Commands...*/
-}
+const BeepScript = require('beepscript'); //Import BeepScript module
+
+const bs = new BeepScript();              //Create BeepScript object
+
+bs.beep(440, 1);                          //Play 400 Hz for 1 second
+bs.sleep(1/2);                            //Sleep for 1/2 second
+bs.note('C#4', 1/4);                      //Play note C#4 for 1/4 second
+bs.sleep(1);                              //Sleep for 1 second
+bs.beep(500, 1/2);                        //Play 500 Hz for 1/2 seconds
+bs.sleep(0.5);                            //Sleep for 0.5 seconds
+bs.note('Eb5', 2);                        //Play note Eb5 for 2 seconds
+
+bs.write('music.wav');                    //Write to file music.wav
+
 ```
+
 # Commands
-- **beep(frequency, duration)** (Duration in seconds, frequency in Hz)
-- **sleep(duration)** (Duration in seconds)
-- **play(sequence, loop)** (Sequence must be betwen quotes)
-- **note(note, octave, duration)** (Note must be betwen quotes, octave is an integer number, duration in secods)
+- **beep(frequency:number = 440, duration:number = 1)**
+- **sleep(duration:number = 1)**
+- **note(note:string = 'A4', duration:number = 1)**
 
- Supported notes: C, C#, D, D#, E, F, F#, G, G#, A, A#, B
-
-# Comments
-```javascript
-/* This is a comment */
+ Supported notes:
 ```
-Comments must be between /* and */
-
-# Example
-
-mymusic.bs
-```
-main {                     /* Main sequence */
-    play('sequence1', 3)   /* Play the sequence1 3 times */
-    beep(600, 1)           /* Beep 600 Hz frequency for 1 second */
-    sleep(0.5)             /* Sleep for 0.5 seconds */
-    play('sequence2', 1)   /* Play the sequence2 one time */
-    note('C#', 3, 0.5)     /* Play the note C# in octave 3 for 0.5 seconds */
-}                          /* Close main sequence */
-
-sequence1 {
-    sleep(0.5)
-    beep(600, 0.3)
-    beep(500, 0.3)
-    beep(400, 0.3)
-    beep(500, 0.3)
-}
-
-sequence2 {
-    note('C', 4, 0.5)
-    note('F#', 3, 1)
-    note('F', 4, 0.3)
-    sleep(2) 
-}
+C0 C#0 Db0 D0 D#0 Eb0 E0 F0 F#0 Gb0 G0 G#0 Ab0 A0 A#0 Bb0 B0
+C1 C#1 Db1 D1 D#1 Eb1 E1 F1 F#1 Gb1 G1 G#1 Ab1 A1 A#1 Bb1 B1
+C2 C#2 Db2 D2 D#2 Eb2 E2 F2 F#2 Gb2 G2 G#2 Ab2 A2 A#2 Bb2 B2
+C3 C#3 Db3 D3 D#3 Eb3 E3 F3 F#3 Gb3 G3 G#3 Ab3 A3 A#3 Bb3 B3
+C4 C#4 Db4 D4 D#4 Eb4 E4 F4 F#4 Gb4 G4 G#4 Ab4 A4 A#4 Bb4 B4
+C5 C#5 Db5 D5 D#5 Eb5 E5 F5 F#5 Gb5 G5 G#5 Ab5 A5 A#5 Bb5 B5
+C6 C#6 Db6 D6 D#6 Eb6 E6 F6 F#6 Gb6 G6 G#6 Ab6 A6 A#6 Bb6 B6
+C7 C#7 Db7 D7 D#7 Eb7 E7 F7 F#7 Gb7 G7 G#7 Ab7 A7 A#7 Bb7 B7
+C8 C#8 Db8 D8 D#8 Eb8 E8 F8 F#8 Gb8 G8 G#8 Ab8 A8 A#8 Bb8 B8
 ```
--- index.js
+++ index.js
@@ -1,105 +1,211 @@
-#!/usr/bin/env node
+const header = require('waveheader');
+const fs = require('fs');
+const path = require('path');
 
-var header = require("waveheader");
-var fs = require("fs");
-var path = require("path");
+module.exports = function(){
 
-var tone = {
-	play: function(freq, lengthInSecs) {
-		freq = freq || 400;
-		lengthInSecs = lengthInSecs || 1;
-		var cycle = Math.round(44100/freq);
-		var samplesLeft = lengthInSecs * 44100;
-		var cycles = samplesLeft/cycle;
-		var ret = [];
-		for(var i = 0; i < cycles; i++) {
-			ret = ret.concat(generateCycle(cycle, 30));
+	let res = [];
+
+	let tone = {
+		beep: function(freq, time) {
+			let cycle = Math.round(44100/freq);
+			let samplesLeft = time * 44100;
+			let cycles = samplesLeft/cycle;
+			let ret = [];
+			for(let i = 0; i < cycles; i++) {
+				ret = ret.concat(generateCycle(cycle, 30));
+			}
+			return ret;
+		},
+		sleep: function(time) {
+			let cycle = Math.round(44100 * time);
+			return generateCycle(cycle, 0);
 		}
-		return ret;
-	},
-	sleep: function(lengthInSecs) {
-		lengthInSecs = lengthInSecs || 1;
-		var cycle = Math.round(44100 * lengthInSecs);
-		return generateCycle(cycle, 0);
-	}
+	};
+
+	let generateCycle = (cycle, volume) => {
+		let data = [];
+		let tmp;
+		for(let i = 0; i < cycle; i++) {
+			tmp = volume * Math.sin((i/cycle) * Math.PI * 2);
+			data[i] = Math.round(tmp);
+		}
+		return data;
+	};
+
+	this.beep = (freq = 440, time = 1) => {
+		res = res.concat(tone.beep(freq, time));
+	};
+
+	this.note = (note = 'A4', time = 1) => {
+		note = note.trim();
+		let notes = {
+			'C0': 16.35,
+			'C#0': 17.32,
+			'Db0': 17.32,
+			'D0': 18.35,
+			'D#0': 19.45,
+			'Eb0': 19.45,
+			'E0': 20.60,
+			'F0': 21.83,
+			'F#0': 23.12,
+			'Gb0': 23.12,
+			'G0': 24.50,
+			'G#0': 25.96,
+			'Ab0': 25.96,
+			'A0': 27.50,
+			'A#0': 29.14,
+			'Bb0': 29.14,
+			'B0': 30.87,
+			'C1': 32.70,
+			'C#1': 34.65,
+			'Db1': 34.65,
+			'D1': 36.71,
+			'D#1': 38.89,
+			'Eb1': 38.89,
+			'E1': 41.20,
+			'F1': 43.65,
+			'F#1': 46.25,
+			'Gb1': 46.25,
+			'G1': 49.00,
+			'G#1': 51.91,
+			'Ab1': 51.91,
+			'A1': 55.00,
+			'A#1': 58.27,
+			'Bb1': 58.27,
+			'B1': 61.74,
+			'C2': 65.41,
+			'C#2': 69.30,
+			'Db2': 69.30,
+			'D2': 73.42,
+			'D#2': 77.78,
+			'Eb2': 77.78,
+			'E2': 82.41,
+			'F2': 87.31,
+			'F#2': 92.50,
+			'Gb2': 92.50,
+			'G2': 98.00,
+			'G#2': 103.83,
+			'Ab2': 103.83,
+			'A2': 110.00,
+			'A#2': 116.54,
+			'Bb2': 116.54,
+			'B2': 123.47,
+			'C3': 130.81,
+			'C#3': 138.59,
+			'Db3': 138.59,
+			'D3': 146.83,
+			'D#3': 155.56,
+			'Eb3': 155.56,
+			'E3': 164.81,
+			'F3': 174.61,
+			'F#3': 185.00,
+			'Gb3': 185.00,
+			'G3': 196.00,
+			'G#3': 207.65,
+			'Ab3': 207.65,
+			'A3': 220.00,
+			'A#3': 233.08,
+			'Bb3': 233.08,
+			'B3': 246.94,
+			'C4': 261.63,
+			'C#4': 277.18,
+			'Db4': 277.18,
+			'D4': 293.66,
+			'D#4': 311.13,
+			'Eb4': 311.13,
+			'E4': 329.63,
+			'F4': 349.23,
+			'F#4': 369.99,
+			'Gb4': 369.99,
+			'G4': 392.00,
+			'G#4': 415.30,
+			'Ab4': 415.30,
+			'A4': 440.00,
+			'A#4': 466.16,
+			'Bb4': 466.16,
+			'B4': 493.88,
+			'C5': 523.25,
+			'C#5': 554.37,
+			'Db5': 554.37,
+			'D5': 587.33,
+			'D#5': 622.25,
+			'Eb5': 622.25,
+			'E5': 659.26,
+			'F5': 698.46,
+			'F#5': 739.99,
+			'Gb5': 739.99,
+			'G5': 783.99,
+			'G#5': 830.61,
+			'Ab5': 830.61,
+			'A5': 880.00,
+			'A#5': 932.33,
+			'Bb5': 932.33,
+			'B5': 987.77,
+			'C6': 1046.50,
+			'C#6': 1108.73,
+			'Db6': 1108.73,
+			'D6': 1174.66,
+			'D#6': 1244.51,
+			'Eb6': 1244.51,
+			'E6': 1318.51,
+			'F6': 1396.91,
+			'F#6': 1479.98,
+			'Gb6': 1479.98,
+			'G6': 1567.98,
+			'G#6': 1661.22,
+			'Ab6': 1661.22,
+			'A6': 1760.00,
+			'A#6': 1864.66,
+			'Bb6': 1864.66,
+			'B6': 1975.53,
+			'C7': 2093.00,
+			'C#7': 2217.46,
+			'Db7': 2217.46,
+			'D7': 2349.32,
+			'D#7': 2489.02,
+			'Eb7': 2489.02,
+			'E7': 2637.02,
+			'F7': 2793.83,
+			'F#7': 2959.96,
+			'Gb7': 2959.96,
+			'G7': 3135.96,
+			'G#7': 3322.44,
+			'Ab7': 3322.44,
+			'A7': 3520.00,
+			'A#7': 3729.31,
+			'Bb7': 3729.31,
+			'B7': 3951.07,
+			'C8': 4186.01,
+			'C#8': 4434.92,
+			'Db8': 4434.92,
+			'D8': 4698.64,
+			'D#8': 4978.03,
+			'Eb8': 4978.03,
+			'E8': 5274.04,
+			'F8': 5587.65,
+			'F#8': 5919.91,
+			'Gb8': 5919.91,
+			'G8': 6271.93,
+			'G#8': 6644.88,
+			'Ab8': 6644.88,
+			'A8': 7040.00,
+			'A#8': 7458.62,
+			'Bb8': 7458.62,
+			'B8': 7902.13
+		};
+		res = res.concat(tone.beep(notes[note], time));
+	};
+
+	this.sleep = (time = 1) => {
+		res = res.concat(tone.sleep(time));
+	};
+
+	this.write = (file = 'music.wav') => {
+		let writer = new fs.createWriteStream(file);
+		writer.write(header());
+		writer.write(new Buffer(res));
+		writer.end();
+	};
+
 };
-
-function generateCycle(cycle, volume) {
-	var data = [];
-	var tmp;
-	for(var i = 0; i < cycle; i++) {
-		tmp = volume * Math.sin((i/cycle) * Math.PI * 2);
-		data[i] = Math.round(tmp);
-	}
-	return data;
-}
-
-function play(index, loop){
-	loop = loop || 1;
-	var res = [];
-	var actions = eval(objeto[index]);
-	for(var i = 0; i < loop; i++){
-		actions.forEach(function(item){
-			res = res.concat(item);
-		});
-	}
-	return res;
-}
-
-function beep(freq, time){
-	return tone.play(freq, time);
-}
-
-function note(nota, octavo, time){
-	if(nota != undefined){
-		octavo = octavo == undefined ? 4 : octavo;
-		nota = nota.split(' ').join('');
-		var notas = {
-			"c": 16,
-			"c#": 17,
-			"d": 18,
-			"d#": 19,
-			"e": 21,
-			"f": 22,
-			"f#": 23,
-			"g": 25,
-			"g#": 26,
-			"a": 27,
-			"a#": 29,
-			"b": 30
-		}
-		var frequencia = notas[nota] * Math.pow(2, octavo);
-		return beep(frequencia, time);
-	}
-	else{
-		return beep(440, 1);
-	}
-}
-
-function sleep(time){
-	return tone.sleep(time);
-}
-
-if(!process.argv[2]){
-	console.log("Usage: beepscript [FILE]");
-}
-else if(!fs.existsSync(process.argv[2])){
-	console.log("No such file or directory "+process.argv[2]);
-}
-else{
-	var res = [];
-	var fileString = fs.readFileSync(process.argv[2], "utf-8");
-	fileString = ('{"'+fileString.toLowerCase().split('\n').join('').split('\t').join('').split(' ').join('').split('"').join("'").split(/\/\*.+?\*\/|\/\/.*(?=[\n\r])/g).join('').split('{').join('":"{').split('{').join('[').split('}').join(']","').split(')').join('),').split(',]').join(']')+'}').split(',"}').join('}');
-	var objeto = JSON.parse(fileString);
-	var main = eval(objeto["main"]);
-	main.forEach(function(item){
-		res = res.concat(item);
-	});
-
-	var wavFile = path.join(path.dirname(process.argv[2]), path.parse(process.argv[2]).name+".wav");
-	var writer = new fs.createWriteStream(wavFile);
-	writer.write(header());
-	writer.write(new Buffer(res));
-	writer.end();
-	console.log("Script compiled to "+wavFile);
-}
